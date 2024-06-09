3cd7f6dd6aa9811b89395304379423a401865993
Lars Bruun-Hansen <lbruun@apache.org>
2024-06-09 11:49:57 +0200

    Initial commit7

--- .github/scripts/maven-execution.sh
+++ .github/scripts/maven-execution.sh
@@ -15,7 +15,7 @@ printenv
 mvn_phase="verify"
 mvn_ci_sha1_short="${GITHUB_SHA::7}"
 mvn_ci_revision=""
-
+mvn_profiles_active=""
 
 
 if [ "GITHUB_REF_TYPE" = "tag" ]; then
@@ -24,6 +24,7 @@ if [ "GITHUB_REF_TYPE" = "tag" ]; then
     mvn_phase="deploy"
     mvn_ci_revision="$GITHUB_REF_NAME"
     mvn_ci_sha1_short=""
+    mvn_profiles_active="-Prelease-to-central"
   else
     # Tagged with something not resembling a SemVer string.
     # This may be a mistake. So we fail!
@@ -33,10 +34,12 @@ if [ "GITHUB_REF_TYPE" = "tag" ]; then
 fi
 
 # Execute maven
+set -x
 mvn \
   --show-version \
   --batch-mode \
   --no-transfer-progress \
   -Dchangelist=  -Dsha1="$mvn_ci_sha1_short"  -Drevision="$mvn_ci_revision" \
+  "$mvn_profiles_active" \
   $mvn_phase
 
