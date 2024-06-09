138b5fa5bbb31fbf0bcaa86af2b24a160ec3a040
Lars Bruun-Hansen <lbruun@apache.org>
2024-06-09 13:56:33 +0200

    Initial commit12

--- .github/scripts/maven-execution.sh
+++ .github/scripts/maven-execution.sh
@@ -9,17 +9,21 @@
 
 set -e
 
-printenv
 
 # Defaults
 mvn_phase="verify"
-mvn_ci_sha1_short="${GITHUB_SHA::7}"
 mvn_ci_revision=""
+mvn_ci_sha1_short="${GITHUB_SHA::7}"
+mvn_ci_changelist=""
 mvn_profiles_active=""
 
-# SemVer regular expression.
-# The tag can optionally start with the 'v' but the 'v' doesn't become
-# part of the Maven version string.
+# SemVer regular expression:
+#  - The tag can optionally start with the 'v' but the 'v' doesn't become part of the
+#    Maven version string.
+# -  We allow the X.Y.Z version to have a pre-release suffix, e.g. "3.2.0-RC1" but if
+#    so we tell Maven that this is a SNAPSHOT release. In other words: tag "3.2.0-RC1" will
+#    be published as "3.2.0-RC1-SNAPSHOT" and will therefore go into the snapshot repo,
+#    not Maven Central.
 #
 semver_regex='^v?([0-9]+\.[0-9]+\.[0-9]+(\-[0-9a-zA-Z.]+)*)$'
 
@@ -27,10 +31,21 @@ if [ "$GITHUB_REF_TYPE" = "tag" ]; then
   # Does tag look like a SemVer string?
   if [[ "$GITHUB_REF_NAME" =~ $semver_regex ]]; then
     semver=${BASH_REMATCH[1]}
+    semver_prerelease=${BASH_REMATCH[2]}
+
     mvn_phase="deploy"
-    mvn_ci_revision="$GITHUB_REF_NAME"
+    mvn_ci_revision="$semver"
     mvn_ci_sha1_short=""
     mvn_profiles_active="-Prelease-to-central"
+
+    # Test for pre-releases. We turn those into SNAPSHOTs
+    if [ ! -z "$semver_prerelease" ]; then
+      # Unless "SNAPSHOT" is already the Semver Pre-release string, then..
+      if [[ ! "$semver_prerelease" =~ SNAPSHOT$ ]]; then
+        mvn_ci_changelist="-SNAPSHOT"  # effectively, this gets appended to the complete Maven version string
+      fi
+    fi
+
   else
     # Tagged with something not resembling a SemVer string.
     # This may be a mistake. So we fail!
@@ -46,8 +61,8 @@ mvn \
   --show-version \
   --batch-mode \
   --no-transfer-progress \
-  -Dchangelist=  -Dsha1=$mvn_ci_sha1_short  -Drevision=$mvn_ci_revision $mvn_profiles_active \
+  -DaltReleaseDeploymentRepository=maven-central::https://oss.sonatype.org/service/local/staging/deploy/maven2 \
+  -DaltSnapshotDeploymentRepository=maven-central::https://oss.sonatype.org/content/repositories/snapshots \
+  -Dchangelist=$mvn_ci_changelist  -Dsha1=$mvn_ci_sha1_short  -Drevision=$mvn_ci_revision  $mvn_profiles_active \
   $mvn_phase
 
-
-echo "End executing mvn"
