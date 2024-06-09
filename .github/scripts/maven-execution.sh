1e4a6c43852fbb572040198f1dc5163301473591
Lars Bruun-Hansen <lbruun@apache.org>
2024-06-09 13:08:45 +0200

    Initial commit11

--- .github/scripts/maven-execution.sh
+++ .github/scripts/maven-execution.sh
@@ -17,10 +17,16 @@ mvn_ci_sha1_short="${GITHUB_SHA::7}"
 mvn_ci_revision=""
 mvn_profiles_active=""
 
+# SemVer regular expression.
+# The tag can optionally start with the 'v' but the 'v' doesn't become
+# part of the Maven version string.
+#
+semver_regex='^v?([0-9]+\.[0-9]+\.[0-9]+(\-[0-9a-zA-Z.]+)*)$'
 
-if [ "GITHUB_REF_TYPE" = "tag" ]; then
+if [ "$GITHUB_REF_TYPE" = "tag" ]; then
   # Does tag look like a SemVer string?
-  if [[ "$GITHUB_REF_NAME" =~ ^[0-9]+\.[0-9]+\.[0-9]+.*$ ]]; then
+  if [[ "$GITHUB_REF_NAME" =~ $semver_regex ]]; then
+    semver=${BASH_REMATCH[1]}
     mvn_phase="deploy"
     mvn_ci_revision="$GITHUB_REF_NAME"
     mvn_ci_sha1_short=""
